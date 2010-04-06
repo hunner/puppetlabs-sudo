327291632328812eb71ded1e91ca92400a630040
Dan Bode <dan@bodepd.com>
2010-04-06 10:27:31 -0500

    added file that tests three distinct sudoers types

---
+++ tests/sudoers-distinct.pp
@@ -0,0 +1,30 @@
+# this will work for all resource types of sudoerd
+resources{'sudoers':
+  purge => true,
+}
+sudo::alias{'BLAH1':
+  #target => '/tmp/sudo',
+  sudo_alias => 'Cmnd_Alias',
+  items => ['/bin/blah', '/bin/blah4', '/bin/blah2'],
+#  require => sudoers['BHAH2']
+}
+sudo::spec{'blah4':
+  #target => '/tmp/sudoers',
+  users => 'dan',
+  hosts => 'localhost',
+  commands => '/bin/true',
+}
+sudo::alias{'BLAH3':
+  sudo_alias => 'Cmnd_Alias',
+  items => ['/bin/blah', '/bin/blah4', '/bin/blah2'],
+  before => Sudoers['BHAH2']
+}
+sudo::alias{'BHAH2':
+  #target => '/tmp/sudoers',
+  sudo_alias => 'Host_Alias',
+  items => ['blah2', 'blah3', 'blah4', 'blah5'],
+#  require => Sudoers['blah4'],
+}
+sudo::defaults{'Defaults@host':
+  parameters => ['x=y', 'one=1', 'two=2'],
+}
