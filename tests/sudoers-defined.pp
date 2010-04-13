e078c19e7c55b4cb65ffbbfdaf5c8276fd29230f
Dan Bode <dan@bodepd.com>
2010-04-13 17:35:24 -0500

    example for using wrapper types

---
+++ tests/sudoers-defined.pp
@@ -0,0 +1,32 @@
+resources{'sudoers':
+  purge => true,
+}
+sudo::alias{'BLAH1':
+  #target => '/tmp/sudoers',
+  type => 'Cmnd_Alias',
+  items => ['/bin/blah', '/bin/blah4', '/bin/blah2'],
+  require => Sudoers['BHAH2']
+}
+sudoers{'blah4':
+  #target => '/tmp/sudoers',
+  ensure => present,
+  users => 'dan',
+  hosts => 'localhost',
+  commands => '/bin/true',
+}
+sudoers{'BLAH3':
+  ensure => present,
+  sudo_alias => 'Cmnd_Alias',
+  items => ['/bin/blah', '/bin/blah4', '/bin/blah2'],
+  before => Sudoers['BHAH2']
+}
+sudoers{'BHAH2':
+  #target => '/tmp/sudoers',
+ ensure => present,
+  sudo_alias => 'Host_Alias',
+  items => ['blah2', 'blah3', 'blah4', 'blah5'],
+  require => Sudoers['blah4'],
+}
+sudoers{'Defaults@host':
+  parameters => ['x=y', 'one=1', 'two=2'],
+}
